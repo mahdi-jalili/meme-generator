7986c96beb3774db3fb64e0d08ac31ec43204d5e
mahdi <mahdi.wise@gmail.com>
2022-08-30 16:23:35 +0430

    i implement the firs version of my meme generator, now you can add image and text and move them like a champ

---
+++ src/components/image/Image.js
@@ -0,0 +1,64 @@
+import { useEffect, useRef } from "react";
+import { Image as ImageKonva, Transformer } from "react-konva";
+import useImage from "use-image";
+
+export default function Image({ shapeProps, onSelect, isSelected, onChange }) {
+    const [image] = useImage(shapeProps.url);
+
+    const shapeRef = useRef();
+    const trRef = useRef();
+
+    useEffect(() => {
+        if (isSelected) {
+            trRef.current.nodes([shapeRef.current]);
+            trRef.current.getLayer().batchDraw();
+        }
+    }, [isSelected]);
+
+    return (
+        <>
+            <ImageKonva
+                image={image}
+                onTap={onSelect}
+                onClick={onSelect}
+                ref={shapeRef}
+                {...shapeProps}
+                draggable
+                onDragEnd={(e) => {
+                    onChange({
+                        ...shapeProps,
+                        x: e.target.x(),
+                        y: e.target.y(),
+                    });
+                }}
+                onTransformEnd={(e) => {
+                    const node = shapeRef.current;
+                    const scaleX = node.scaleX();
+                    const scaleY = node.scaleY();
+
+                    node.scaleX(1);
+                    node.scaleY(1);
+                    onChange({
+                        ...shapeProps,
+                        x: node.x(),
+                        y: node.y(),
+                        width: Math.max(5, node.width() * scaleX),
+                        height: Math.max(node.height() * scaleY),
+                    });
+                }}
+            ></ImageKonva>
+            {isSelected && (
+                <Transformer
+                    ref={trRef}
+                    boundBoxFunc={(oldBox, newBox) => {
+                        // limit resize
+                        if (newBox.width < 5 || newBox.height < 5) {
+                            return oldBox;
+                        }
+                        return newBox;
+                    }}
+                ></Transformer>
+            )}
+        </>
+    );
+}
---
+++ src/components/text/Text.js
@@ -0,0 +1,61 @@
+import { useEffect, useRef } from "react";
+import { Text as TextKonva, Transformer } from "react-konva";
+// import style from "./text.module.css";
+
+export default function Text({ shapeProps, onSelect, isSelected, onChange }) {
+    const shapeRef = useRef();
+    const trRef = useRef();
+
+    useEffect(() => {
+        if (isSelected) {
+            trRef.current.nodes([shapeRef.current]);
+            trRef.current.getLayer().batchDraw();
+        }
+    }, [isSelected]);
+
+    return (
+        <>
+            <TextKonva
+                onTap={onSelect}
+                onClick={onSelect}
+                ref={shapeRef}
+                {...shapeProps}
+                draggable
+                onDragEnd={(e) => {
+                    onChange({
+                        ...shapeProps,
+                        x: e.target.x(),
+                        y: e.target.y(),
+                    });
+                }}
+                onTransformEnd={(e) => {
+                    const node = shapeRef.current;
+                    const scaleX = node.scaleX();
+                    const scaleY = node.scaleY();
+
+                    node.scaleX(1);
+                    node.scaleY(1);
+                    onChange({
+                        ...shapeProps,
+                        x: node.x(),
+                        y: node.y(),
+                        width: Math.max(5, node.width() * scaleX),
+                        height: Math.max(node.height() * scaleY),
+                    });
+                }}
+            ></TextKonva>
+            {isSelected && (
+                <Transformer
+                    ref={trRef}
+                    boundBoxFunc={(oldBox, newBox) => {
+                        // limit resize
+                        if (newBox.width < 5 || newBox.height < 5) {
+                            return oldBox;
+                        }
+                        return newBox;
+                    }}
+                ></Transformer>
+            )}
+        </>
+    );
+}
